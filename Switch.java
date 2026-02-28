1676786dd7079ca69707d328c9105d04b5799b93
Lokanatha-Reddy817 <maddurinani74367@gmail.com>
2026-02-28 20:15:08 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,13 @@
+public class Switch{
+    public static void main(String[] args){
+        String day="Tuesday";
+        switch(day){
+            case "Tuesday" -> System.out.println("6am");
+            case "Monday" ->System.out.println("6am");
+            default ->System.out.println("4am");
+
+
+
+        }
+    }
+}
